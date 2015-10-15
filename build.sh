2c262103733c7e3d29630b0e6049c9b2ce6ed23f
Bernat Gaston <bernat.gaston@i2cat.net>
2015-10-15 16:41:13 +0200

    Rename generated binary
    Generated binary has been renamed to avoid replacing c++ Manager

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 find -name "*.java" > sources.txt
 javac -d bin -cp /home/irati/iratis/share/librina/librina.jar:/home/irati/iratis/share/rinad/librinad.jar @sources.txt
-jar cfm ../iratis/bin/manager manifest.mf bin
+jar cfm ../iratis/bin/manager_java manifest.mf bin
 rm sources.txt
