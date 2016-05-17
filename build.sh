34634a28cad731fe36b65cdee4c89d1583f62902
Bernat <bernat.gaston@i2cat.net>
2016-05-17 14:04:18 +0200

    Adapted

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 find -name "*.java" > sources.txt
-javac -d bin -cp /home/irati/irati/share/librina/librina.jar:/home/irati/irati/share/rinad/librinad.jar @sources.txt
-jar cfm ../irati/bin/manager_java manifest.mf bin
+javac -d bin -cp /root/irati/share/librina/librina.jar:/root/irati/share/rinad/librinad.jar @sources.txt
+jar cfm /root/irati/bin/manager_java manifest.mf bin
 rm sources.txt
